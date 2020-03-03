ddbe9e079a53cacaae9ce8d3d1c3630212dbabab
unknown <eloso85@gmail.com>
2020-03-03 09:44:10 -0600

    created data

---
+++ src/EmployeeData.js
@@ -0,0 +1,107 @@
+const employeeData =[
+    {
+        id:1,
+        name: "Alejandro",
+        position: "Manager",
+        department: "Store",
+        salary: 100000
+    },
+
+    {
+        id:2,
+        name: "Bert",
+        position:"Part-Time",
+        department:"seasonal",
+        salary:20000
+    },
+
+    {
+        id:3,
+        name: "Daniela",
+        position:"Loss-Prevention",
+        department:"LP",
+        salary:34000
+    },
+
+    {
+        id:4,
+        name:"Peter",
+        position:"Assistant Manager",
+        department:"Store",
+        salary:60000
+    },
+
+    {
+        id:5,
+        name:"David",
+        position:"Full-Time",
+        department:"Womens",
+        salary:24000
+    },
+
+    {
+        id:6,
+        name:"Tina",
+        position:"Department-Lead",
+        department:"Mens",
+        salary:35000
+    },
+
+    {
+        id:7,
+        name:"Juan",
+        position:"Part-Time",
+        department:"Home",
+        salary:24000
+    },
+
+    {
+        id:8,
+        name:"Alfredo",
+        position:"Department-Lead",
+        department:"Kids",
+        salary:34000
+    },
+
+    {
+        id:8,
+        name:"Nolan",
+        position:"Department-Lead",
+        department:"Shoes",
+        salary:28000
+    },
+
+    {
+        id:9,
+        name:"Analisa",
+        position:"Part-Time",
+        department:"Kids",
+        salary:15000
+    },
+
+    {
+        id:10,
+        name:"Janney",
+        position:"Full-time",
+        department:"Mens",
+        salary:13000
+    },
+
+    {
+        id:11,
+        name:"Deidra",
+        position:"Full-time",
+        department:"Womens",
+        salary:19000
+    },
+
+    {
+        id:12,
+        name:"Julia",
+        position:"Manager",
+        department:"Store",
+        salary:78000
+    }
+]
+
+export default employeeData
---
+++ src/Table.js
@@ -0,0 +1,54 @@
+import React from 'react'
+import * as ReactBootStrap from "react-bootstrap"
+import EmployeeData from "./EmployeeData"
+
+function Table(){
+    return (
+        <div>
+            <ReactBootStrap.Table responsive>
+  <thead>
+    <tr>
+      <th>#</th>
+      <th>Name</th>
+      <th>Postion</th>
+      <th>Department</th>
+      <th>Salary</th>
+      <th></th>
+      <th></th>
+    </tr>
+  </thead>
+  <tbody>
+    <tr>
+      <td>1</td>
+      <td>Table cell</td>
+      <td>Table cell</td>
+      <td>Table cell</td>
+      <td>Table cell</td>
+      <td>Table cell</td>
+      <td>Table cell</td>
+    </tr>
+    <tr>
+      <td>2</td>
+      <td>Table cell</td>
+      <td>Table cell</td>
+      <td>Table cell</td>
+      <td>Table cell</td>
+      <td>Table cell</td>
+      <td>Table cell</td>
+    </tr>
+    <tr>
+      <td>3</td>
+      <td>Table cell</td>
+      <td>Table cell</td>
+      <td>Table cell</td>
+      <td>Table cell</td>
+      <td>Table cell</td>
+      <td>Table cell</td>
+    </tr>
+  </tbody>
+</ReactBootStrap.Table>
+        </div>
+    )
+}
+
+export default Table
