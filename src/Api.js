5ed612b9b85c1af7f9259be1f62a25541729d935
unknown <eloso85@gmail.com>
2020-03-04 19:17:50 -0600

    ugh

--- src/Api.js
+++ src/Api.js
@@ -1,6 +1,6 @@
 import React, {Component} from "react"
 import Axios from "axios"
-
+//import * as ReactBootStrap from "react-bootstrap"
 class Api extends Component {
     
         
@@ -10,10 +10,10 @@ class Api extends Component {
         }
     
     componentDidMount(){
-       Axios.get('https://randomuser.me/api/?results=50') 
+       Axios.get('https://randomuser.me/api/?results=10') 
        .then((res)=>{
             console.log(res)
-            const repos= res.data.results
+            const repos= res.data.results.map(person=> person = {person})
             console.log(repos)
             this.setState({character:repos})
             
@@ -21,18 +21,26 @@ class Api extends Component {
        
     }
     render(){
+        
         return(
             <div>
-                {this.state.character.map(person => (
-                    
-                    <div key = {person.login.uuid}>
+                {this.character
+
+                
+                /* {this.state.character.map(person => (
                    
-                        <div>{person.name.first}</div> 
+                    <div key = {person.login.uuid}>
+                        
+                        <div>{person.name.first}</div>
+                        <div>{person.name.last}</div>
                         <img src ={person.picture.large} alt=""/>
+                        <div>{person.phone}</div>
+                        <div>{person.email}</div>
                    </div>
-                
-                
-                ))}
+                   
+                ))} */}
+
+                    
             </div>
         )
     }
--- src/Table.js
+++ src/Table.js
@@ -1,37 +1,16 @@
 import React from 'react'
-import * as ReactBootStrap from "react-bootstrap"
+//import * as ReactBootStrap from "react-bootstrap"
 import Api from "./Api"
 //import EmployeeData from "./EmployeeData"
-console.log(Api)
-function Table(props){
+
+function Table(){
     
     return (
         <div>
-            <ReactBootStrap.Table responsive>
-              
-  <thead>
-    <tr>
-      <th>Picture</th>
-      <th>Name</th>
-      <th>Postion</th>
-      <th>Department</th>
-      <th>Salary</th>
-      
-    </tr>
-  </thead>
-  <tbody>
-    <tr>
-      
-      <td></td>
-      <td>{Api.name}</td>
-      <td>{props.postion}</td>
-      <td>{props.department}</td>
-      <td>{props.salary}</td>
-     
-    </tr>
-   
-  </tbody>
-</ReactBootStrap.Table>
+            <Api />
+            
+            
+  
         </div>
     )
 }
