fa47573f1cb6aab994198058fd26249358d10b63
unknown <eloso85@gmail.com>
2020-03-06 22:21:38 -0600

    adding

--- src/App.js
+++ src/App.js
@@ -1,20 +1,23 @@
-import React from 'react';
+import React, { Component } from 'react';
 //import logo from './logo.svg';
 import './App.css';
 //import * as ReactBootStrap from "react-bootstrap"
-import Table from './Table'
+import Api from './Api'
+import Search from './SearchBar';
 //import EmployeeData from "./EmployeeData"
 
 
-function App() {
-  
+class App extends Component {
+  render(){
   // const employeeComponent = EmployeeData.map( employee => <Table key ={employee.id} name ={employee.name} />)
   // console.log(employeeComponent)
   return (
     <div>
-      <Table />
+      <Search />
+      <Api />
     </div>
   );
+  }
 }
 
 export default App;
---
+++ src/SearchBar.js
@@ -0,0 +1,39 @@
+import React, { Component } from "react"
+import {Jumbotron,Nav,Form,FormControl,Button, Navbar} from "react-bootstrap"
+
+
+class Search extends Component {
+render(){
+    return(
+        <div>
+          
+            
+        <Jumbotron>
+  <h1>Hello, world!</h1>
+  <p>
+    This is a simple hero unit, a simple jumbotron-style component for calling
+    extra attention to featured content or information.
+  </p>
+  <p>
+    
+  </p>
+  <Navbar bg="primary" variant="dark">
+    <Navbar.Brand href="#home">Navbar</Navbar.Brand>
+    <Nav className="mr-auto">
+      <Nav.Link href="#home">Home</Nav.Link>
+      <Nav.Link href="#features">Features</Nav.Link>
+      <Nav.Link href="#pricing">Pricing</Nav.Link>
+    </Nav>
+    <Form inline>
+      <FormControl type="text" placeholder="Search" className="mr-sm-2" />
+      <Button variant="outline-light">Search</Button>
+    </Form>
+  </Navbar>
+</Jumbotron>
+
+        </div>
+    )
+}
+}
+
+export default Search
